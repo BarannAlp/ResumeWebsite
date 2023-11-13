983a0eafec9183934af5e4acaa756e598c7b96ff
Baran <126600817+BarannAlp@users.noreply.github.com>
2023-11-13 12:43:09 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons'
+import {HiArrowNarrowRight} from 'react-icons/hi'
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'> Home</div>
+    <div name='Home' className='w-full h-screen bg-[#0a192f]'> 
+    {/*Container */}
+<div className=' max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+<p className='text-white'> Welcome to my Website! I am,</p>
+<h1 className='text-4xl sm:text-7xl font-bold text-yellow-600'>Baran Alp Oduncu</h1>
+<h2 className='text-4xl sm:text-7xl font-bold text-yellow-800'>I am a Full Stack Developer,</h2>
+<p className='text-white py-4 max-w-[700px]'>I hold a Bachelor's degree in Electrical and Electronic Engineering and am completing an additional major in Software Engineering at Bahcesehir University. 
+  My academic journey equips me with strong engineering fundamentals,
+   while my passion for technology drives my focus on software development to apply creative and problem-solving skills.</p>
+<div>
+  
+<button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600'> View Work
+<span className='group-hover:rotate-90 duration-300'> <HiArrowNarrowRight className='ml-3 hover'/>  </span></button>
+
+</div>
+</div>
+
+    </div >
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,8 @@ const Navbar = () => {
   const [IsNav,setIsNav]=useState(false);
   const handleClick = () => setIsNav(!IsNav)
   return (
-<div class='text-gray-300 text-2xl fixed top-0 w-full h-20 flex justify-between items-center px-4 bg-[#060c16]'>
-<div>
-<img  alt="Logo Image" style={{width: '50px'}}/>
-</div>
+<div class='text-gray-300 text-2xl fixed top-0 w-full  flex justify-between items-center pr-4 bg-[#060c16]'>
+<img className='mt-[-15px]' src={Logo} alt="Logo Image" style={{width: '120px'}}/>
 {/* Pages */}
   <ul className='hidden md:flex'>
     <li>Home</li>
@@ -37,11 +35,11 @@ const Navbar = () => {
 
 {/* Icons */}
 
-<ul className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-blue-600'> <a className='flex justify-between items-center w-full text-gray-300' hrefLang='' >LinkedIn<FaLinkedin size={30}/></a></li>
+<ul className=' hidden lg:flex fixed flex-col top-[35%] left-0'>
+  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-blue-700'> <a className='flex justify-between items-center w-full text-gray-300' hrefLang='' >LinkedIn<FaLinkedin size={30}/></a></li>
   <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-black'> <a className='flex justify-between items-center w-full text-gray-300' hrefLang='' >GitHub<FaGithub size={30}/></a></li>
   <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-yellow-500'> <a className='flex justify-between items-center w-full text-gray-300' hrefLang='' >Resume<BsFillPersonLinesFill size={30}/></a></li>
-  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-red-600'> <a className='flex justify-between items-center w-full text-gray-300' hrefLang='' >Email<HiOutlineMail size={30}/></a></li>
+  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-red-700'> <a className='flex justify-between items-center w-full text-gray-300' hrefLang='' >Email<HiOutlineMail size={30}/></a></li>
 </ul>
 
 </div>
